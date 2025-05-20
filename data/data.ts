9a8d04c31769548be65a330ac7130153585bad6c
Fuchsius (PVT) Ltd <dev.kalanadidulanga@gmail.com>
2025-05-20 10:52:50 +0530

    data

---
+++ data/data.ts
@@ -0,0 +1,12 @@
+export const CONTACT_DETAILS = {
+  numbers: ["+94 112 795 603", "+94 77 7 825 062"],
+  email: ["support@fuchsisus.com"],
+  address: "Sir Baron Jayathilake Mawatha ,Colombo, Sri Lanka",
+};
+
+export const SOCIAL_MEDIA = {
+  facebook: "https://www.facebook.com/fuchsisus",
+  instagram: "https://www.instagram.com/fuchsius_official",
+  tiktok: "https://www.tiktok.com/@fuchsiusofficial",
+  linkedin: "https://www.linkedin.com/company/fuchsius",
+};
